43e18a79253d4cf07e9602b2e98e2372e27e318d
elias-a <elias.alstead@gmail.com>
2022-05-26 23:37:40 -0400

    Read input parameters from a file

--- simulation/falling-u.cpp
+++ simulation/falling-u.cpp
@@ -1,22 +1,32 @@
-#include <cmath>
-#include <iostream>
+#include <fstream>
 #include "u.h"
 #include "simulation.h"
 
 int main() {
     // Dimensions of the U
-    double baseLength_m = 0.08;
-    double sideHeight_m = 0.11;
+    double baseLength_m;
+    double sideHeight_m;
 
     // Initial conditions
-    double position_m = 1.;
-    double velocity_m_s = 0.;
-    double angular_position_rad = 0.;
-    double angular_velocity_rad_s = 0.;
+    double position_m;
+    double velocity_m_s;
+    double angular_position_rad;
+    double angular_velocity_rad_s;
 
     // Simulation variables
-    double timeStep_s = 1e-6;
-    int granularity = 1000;
+    double timeStep_s;
+    int granularity;
+
+    std::ifstream file("parameters.txt");
+    file >> baseLength_m;
+    file >> sideHeight_m;
+    file >> position_m;
+    file >> velocity_m_s;
+    file >> angular_position_rad;
+    file >> angular_velocity_rad_s;
+    file >> timeStep_s;
+    file >> granularity;
+    file.close();
 
     U u(baseLength_m, sideHeight_m, position_m, velocity_m_s, angular_position_rad, angular_velocity_rad_s);
     u.writeDimensions();
